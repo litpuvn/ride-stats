ac8dc73d0d20a6eb26f65641d70b3c44ff2de25b
Hoang Long <litpuvn@gmail.com>
2017-03-15 22:35:19 -0500

    first attemp to introduce unit test in android

--- CheapRide/app/src/main/java/edu/ttu/spm/cheapride/LoginActivity.java
+++ CheapRide/app/src/main/java/edu/ttu/spm/cheapride/LoginActivity.java
@@ -177,7 +177,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
+    public void attemptLogin() {
         if (mAuthTask != null) {
             return;
         }
