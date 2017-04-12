d6dfee0a914367a0b8e8124d37cfd4a7e4cdd4d3
Charlie441324219 <dian.li@ttu.edu>
2017-04-12 15:49:06 -0500

    start new branch

---
+++ CheapRide/app/src/main/java/edu/ttu/spm/cheapride/activity_rideHistory.java
@@ -0,0 +1,13 @@
+package edu.ttu.spm.cheapride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_rideHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_history);
+    }
+}
