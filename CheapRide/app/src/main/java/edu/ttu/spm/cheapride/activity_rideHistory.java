0e560b9502ab241d99846f4be4420ae1928c765f
Charlie441324219 <dian.li@ttu.edu>
2017-04-29 19:21:27 -0500

    app crash

--- CheapRide/app/src/main/java/edu/ttu/spm/cheapride/activity_rideHistory.java
+++ CheapRide/app/src/main/java/edu/ttu/spm/cheapride/activity_rideHistory.java
@@ -2,6 +2,7 @@ package edu.ttu.spm.cheapride;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,11 +14,13 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import static edu.ttu.spm.cheapride.R.id.history_table;
@@ -41,6 +44,16 @@ public class activity_rideHistory extends AppCompatActivity {
     int DIALOG_ID = 0;
 
     private Button date_submit;
+    Pageable<historyRecord> pageableArray;
+
+    TextView pageCounter;
+    Button nextPage;
+    Button previousPage;
+    TableRow tableRow;
+    LinearLayout separator;
+    TextView value;
+
+    ArrayList<historyRecord> historyRecordArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +67,8 @@ public class activity_rideHistory extends AppCompatActivity {
         showDialogOnTextViewClick();
 
         //insert new rows
-        insertRow();
+        historyTable();
+
     }
 
     public void setDefaultDate(){
@@ -144,10 +158,127 @@ public class activity_rideHistory extends AppCompatActivity {
         }
     };
 
-    private void insertRow(){
+    //create history record class
+    public class historyRecord{
+        private String number;
+        private String date;
+        private String provider;
+        private String pick;
+        private String destination;
+        private String fee;
+
+        public historyRecord(String number,String date,String provider,String pick,String destination,String fee){
+            this.number = number;
+            this.date = date;
+            this.provider = provider;
+            this.pick = pick;
+            this.destination = destination;
+            this.fee = fee;
+        }
+        public String getNumber(){return number;}
+        public String getDate(){return date;}
+        public String getProvider(){return provider;}
+        public String getPick(){return pick;}
+        public String getDestination(){return destination;}
+        public String getFee(){return fee;}
+    }
+
+    private void historyTable(){
+        pageCounter  = (TextView) findViewById(R.id.pageCounter_history);
+        nextPage = (Button) findViewById(R.id.nextButton_history);
+        previousPage = (Button) findViewById(R.id.previousButten_history);
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pageableArray.setPage(pageableArray.getNextPage());
+//                tableRow.removeAllViews();
+                displayPage();
+                pageCounter.setText("Page " + pageableArray.getPage() + " of " + pageableArray.getMaxPages());
+            }
+        });
+
+        previousPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pageableArray.setPage(pageableArray.getPreviousPage());
+//                tableRow.removeAllViews();
+                displayPage();
+                pageCounter.setText("Page " + pageableArray.getPage() + " of " + pageableArray.getMaxPages());
+
+            }
+        });
+
+        for(int i = 0; i <= 100; i++){
+            //here to input the content of list
+            historyRecordArrayList.add(new historyRecord(""+i+1,"02/15/2017","Uber","2606 31street 79410","33 University Ave.79210","$2"));
+        }
+
+        pageableArray = new Pageable<>(historyRecordArrayList);
+        pageableArray.setPageSize(20);
+        pageableArray.setPage(1);
+        pageCounter.setText("Page " + pageableArray.getPage() + " of " + pageableArray.getMaxPages());
+        displayPage();
+    }
+
+    public void displayPage(){
+        TableLayout historyTable = (TableLayout) findViewById(history_table);
+        addTitle();
+
+        for (historyRecord v : pageableArray.getListForPage()) {
+            //create a new row
+            TableRow newRow = new TableRow(this);
+
+            //create text view to add number
+            TextView labelNUMBER = new TextView(this);
+            labelNUMBER.setText(v.getNumber());
+            labelNUMBER.setGravity(Gravity.CENTER);
+            newRow.addView(labelNUMBER);
+
+            //create text view to add date
+            TextView labelDATE = new TextView(this);
+            labelDATE.setText(String.valueOf(v.getDate()));
+            labelDATE.setGravity(Gravity.CENTER);
+            newRow.addView(labelDATE);
+
+            //create text view to add provide
+            TextView labelPROVIDE = new TextView(this);
+            labelPROVIDE.setText(String.valueOf(v.getProvider()));
+            labelPROVIDE.setGravity(Gravity.CENTER);
+            newRow.addView(labelPROVIDE);
+
+            //create text view to add pickup
+            TextView labelPICKUP = new TextView(this);
+            labelPICKUP.setText(String.valueOf(v.getPick()));
+            labelPICKUP.setGravity(Gravity.CENTER);
+            newRow.addView(labelPICKUP);
+
+            //create text view to add destination
+            TextView labelDESTINATION = new TextView(this);
+            ;
+            labelDESTINATION.setText(String.valueOf(v.getDate()));
+            labelDESTINATION.setGravity(Gravity.CENTER);
+            newRow.addView(labelDESTINATION);
+
+            //create text view to add fee
+            TextView labelFEE = new TextView(this);
+            labelFEE.setText(String.valueOf(v.getFee()));
+            labelFEE.setGravity(Gravity.CENTER);
+            newRow.addView(labelFEE);
+
+            //add new row into table
+            historyTable.addView(newRow);
+
+           // addSeparator();
+
+        }
+    }
+
+
+    public void addTitle(){
         String numberTitle = "  No. ";
         String dateTitle = "  Date  ";
-        String provideTitle = "   Provider   ";
+        String providerTitle = "   Provider   ";
         String pickupTitle = "      Pickup      ";
         String destinationTitle = "      Destination      ";
         String feeTitle = "  Fee  ";
@@ -169,7 +300,7 @@ public class activity_rideHistory extends AppCompatActivity {
 
         //add title for provide
         TextView title2 = new TextView(this);
-        title2.setText(provideTitle);
+        title2.setText(providerTitle);
         titleRow.addView(title2);
 
         //add title for pickup
@@ -189,58 +320,118 @@ public class activity_rideHistory extends AppCompatActivity {
 
         historyTable.addView(titleRow);
 
-        for(int i = 1; i <100; i++){
-
-            //int number = i ;
-            String date = "02/15/2017";
-            String provide = "Uber";
-            String pickup = "2606 31street 79410";
-            String destination = "33 University Ave.79210";
-            String fee = "$2";
-
-            //create a new row
-            TableRow newRow = new TableRow(this);
-
-            //create text view to add number
-            TextView labelNUMBER = new TextView(this);
-            labelNUMBER.setText("" + i);
-            labelNUMBER.setGravity(Gravity.CENTER);
-            newRow.addView(labelNUMBER);
-
-            //create text view to add date
-            TextView labelDATE  = new TextView(this);
-            labelDATE.setText(date);
-            labelDATE.setGravity(Gravity.CENTER);
-            newRow.addView(labelDATE);
-
-            //create text view to add provide
-            TextView labelPROVIDE = new TextView(this);
-            labelPROVIDE.setText(provide);
-            labelPROVIDE.setGravity(Gravity.CENTER);
-            newRow.addView(labelPROVIDE);
-
-            //create text view to add pickup
-            TextView labelPICKUP = new TextView(this);
-            labelPICKUP.setText(pickup);
-            labelPICKUP.setGravity(Gravity.CENTER);
-            newRow.addView(labelPICKUP);
-
-            //create text view to add destination
-            TextView labelDESTINATION = new TextView(this);;
-            labelDESTINATION.setText(destination);
-            labelDESTINATION.setGravity(Gravity.CENTER);
-            newRow.addView(labelDESTINATION);
-
-            //create text view to add fee
-            TextView labelFEE = new TextView(this);
-            labelFEE.setText(fee);
-            labelFEE.setGravity(Gravity.CENTER);
-            newRow.addView(labelFEE);
-
-            //add new row into table
-            historyTable.addView(newRow);
-        }
     }
+
+//    private void addSeparator() {
+//        Resources res = MainActivity.this.getResources();
+//        separator = new LinearLayout(MainActivity.this);
+//        separator.setOrientation(LinearLayout.VERTICAL);
+//        separator.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 2));
+//        separator.setBackgroundColor(Color.parseColor("#5e7974"));
+//        separator.setDividerDrawable(res.getDrawable(R.drawable.radius_middle));
+//        maintable.addView(separator);
+//
+//    }
 }
+
+
+
+//        String numberTitle = "  No. ";
+//        String dateTitle = "  Date  ";
+//        String providerTitle = "   Provider   ";
+//        String pickupTitle = "      Pickup      ";
+//        String destinationTitle = "      Destination      ";
+//        String feeTitle = "  Fee  ";
+//
+//        TableLayout historyTable = (TableLayout) findViewById(history_table);
+//
+//        //create row for title
+//        TableRow titleRow = new TableRow(this);
+//
+//        //add title for number
+//        TextView title0 = new TextView(this);
+//        title0.setText(numberTitle);
+//        titleRow.addView(title0);
+//
+//        //add title for date
+//        TextView title1 = new TextView(this);
+//        title1.setText(dateTitle);
+//        titleRow.addView(title1);
+//
+//        //add title for provide
+//        TextView title2 = new TextView(this);
+//        title2.setText(providerTitle);
+//        titleRow.addView(title2);
+//
+//        //add title for pickup
+//        TextView title3 = new TextView(this);
+//        title3.setText(pickupTitle);
+//        titleRow.addView(title3);
+//
+//        //add title for destination
+//        TextView title4 = new TextView(this);
+//        title4.setText(destinationTitle);
+//        titleRow.addView(title4);
+//
+//        //add title for fee
+//        TextView title5 = new TextView(this);
+//        title5.setText(feeTitle);
+//        titleRow.addView(title5);
+//
+//        historyTable.addView(titleRow);
+//
+////        for(int i = 1; i <100; i++){
+////
+////            //int number = i ;
+////            String date = "02/15/2017";
+////            String provide = "Uber";
+////            String pickup = "2606 31street 79410";
+////            String destination = "33 University Ave.79210";
+////            String fee = "$2";
+////
+////            //create a new row
+////            TableRow newRow = new TableRow(this);
+////
+////            //create text view to add number
+////            TextView labelNUMBER = new TextView(this);
+////            labelNUMBER.setText("" + i);
+////            labelNUMBER.setGravity(Gravity.CENTER);
+////            newRow.addView(labelNUMBER);
+////
+////            //create text view to add date
+////            TextView labelDATE  = new TextView(this);
+////            labelDATE.setText(date);
+////            labelDATE.setGravity(Gravity.CENTER);
+////            newRow.addView(labelDATE);
+////
+////            //create text view to add provide
+////            TextView labelPROVIDE = new TextView(this);
+////            labelPROVIDE.setText(provide);
+////            labelPROVIDE.setGravity(Gravity.CENTER);
+////            newRow.addView(labelPROVIDE);
+////
+////            //create text view to add pickup
+////            TextView labelPICKUP = new TextView(this);
+////            labelPICKUP.setText(pickup);
+////            labelPICKUP.setGravity(Gravity.CENTER);
+////            newRow.addView(labelPICKUP);
+////
+////            //create text view to add destination
+////            TextView labelDESTINATION = new TextView(this);;
+////            labelDESTINATION.setText(destination);
+////            labelDESTINATION.setGravity(Gravity.CENTER);
+////            newRow.addView(labelDESTINATION);
+////
+////            //create text view to add fee
+////            TextView labelFEE = new TextView(this);
+////            labelFEE.setText(fee);
+////            labelFEE.setGravity(Gravity.CENTER);
+////            newRow.addView(labelFEE);
+////
+////            //add new row into table
+////            historyTable.addView(newRow);
+////        }
+//    }
+//}
 
 
