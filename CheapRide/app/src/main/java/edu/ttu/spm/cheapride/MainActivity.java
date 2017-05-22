f03b5fb6227af3164095c75f3d6bd8c7d36cc76b
Charlie441324219 <dian.li@ttu.edu>
2017-05-22 00:54:31 -0500

    implement the chart

--- CheapRide/app/src/main/java/edu/ttu/spm/cheapride/MainActivity.java
+++ CheapRide/app/src/main/java/edu/ttu/spm/cheapride/MainActivity.java
@@ -17,10 +17,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -51,12 +56,14 @@ import edu.ttu.spm.cheapride.handler.BookingHandler;
 import edu.ttu.spm.cheapride.handler.EstimateHandler;
 import edu.ttu.spm.cheapride.listener.MyPlaceSelectionListener;
 import edu.ttu.spm.cheapride.model.BookResponse;
+import edu.ttu.spm.cheapride.model.NightingaleRoseChart;
 import edu.ttu.spm.cheapride.model.RideEstimate;
 import edu.ttu.spm.cheapride.model.RideEstimateDTO;
 import edu.ttu.spm.cheapride.model.RideEstimateRequest;
 import edu.ttu.spm.cheapride.model.item.Driver;
 import edu.ttu.spm.cheapride.model.item.Vehicle;
 import edu.ttu.spm.cheapride.service.TrackGPS;
+import edu.ttu.spm.cheapride.view.DemoView;
 
 
 public class MainActivity extends AppCompatActivity
@@ -159,6 +166,9 @@ public class MainActivity extends AppCompatActivity
     }
 
     public static Geocoder geocoder;
+
+    private DemoView mCharts;
+    private LinearLayout RoseChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -437,8 +447,10 @@ public class MainActivity extends AppCompatActivity
     public void onRegisterClicked(View v) {
         System.out.println("register clicked");
 
-        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+//        startActivity(intent);
+
+        initRoseChart();
     }
 
     public LatLng getCurrentLocation() {
@@ -591,6 +603,69 @@ public class MainActivity extends AppCompatActivity
         }
 
         return CAR_TYPE_MAP.get(this.selectedCarType);
+    }
+
+    public void initRoseChart()
+    {
+        //图表的使用方法:
+        //使用方式一:
+        // 1.新增一个Activity
+        // 2.新增一个View,继承Demo中的GraphicalView或DemoView都可，依Demo中View目录下例子绘制图表.
+        // 3.将自定义的图表View放置入Activity对应的XML中，将指明其layout_width与layout_height大小.
+        // 运行即可看到效果. 可参考非ChartsActivity的那几个图的例子，都是这种方式。
+
+        //使用方式二:
+        //代码调用 方式有下面二种方法:
+        //方法一:
+        //在xml中的FrameLayout下增加图表和ZoomControls,这是利用了现有的xml文件.
+        // 1. 新增一个View，绘制图表.
+        // 2. 通过下面的代码得到控件，addview即可
+        //LayoutInflater factory = LayoutInflater.from(this);
+        //View content = (View) factory.inflate(R.layout.activity_multi_touch, null);
+
+
+        //方法二:
+        //完全动态创建,无须XML文件.
+        FrameLayout content = new FrameLayout(this);
+
+        //缩放控件放置在FrameLayout的上层，用于放大缩小图表
+        FrameLayout.LayoutParams frameParm = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        frameParm.gravity = Gravity.BOTTOM|Gravity.RIGHT;
+
+		   /*
+		  //缩放控件放置在FrameLayout的上层，用于放大缩小图表
+	       mZoomControls = new ZoomControls(this);
+	       mZoomControls.setIsZoomInEnabled(true);
+	       mZoomControls.setIsZoomOutEnabled(true);
+		   mZoomControls.setLayoutParams(frameParm);
+		   */
+
+        //图表显示范围在占屏幕大小的90%的区域内
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int scrWidth = (int) (dm.widthPixels * 0.5);
+        int scrHeight = (int) (dm.heightPixels * 0.5);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                scrWidth, scrHeight);
+
+        //居中显示
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        //图表view放入布局中，也可直接将图表view放入Activity对应的xml文件中
+        //final RelativeLayout chartLayout = new RelativeLayout(this);
+        RoseChart = (LinearLayout)findViewById(R.id.rose_chart);
+
+        mCharts = new NightingaleRoseChart(this);
+
+        RoseChart.addView( mCharts, layoutParams);
+
+        //增加控件
+        //((ViewGroup) content).addView(RoseChart);
+        //((ViewGroup) content).addView(mZoomControls);
+        //setContentView(content);
+        //放大监听
+        //  mZoomControls.setOnZoomInClickListener(new OnZoomInClickListenerImpl());
+        //缩小监听
+        //  mZoomControls.setOnZoomOutClickListener(new OnZoomOutClickListenerImpl());
     }
 }
 
